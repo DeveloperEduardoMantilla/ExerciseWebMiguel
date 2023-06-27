107a32efd6693676f47c50cb2a3104aa89afa9f2
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-06-27 18:31:30 -0500

    feat(Ejercicios Php): :sparkles: Ejercicio 2
    Solicion frente a la problematica planteada en el ejercicio numero 2.

--- app.php
+++ app.php
@@ -1,19 +1,6 @@
-
 <?php
-/*The classical introductory exercise. Just say "Hello, World!".
-
-["Hello, World!"](https://en.wikipedia.org/wiki/"Hello,_world!"_program) is the traditional first program for beginning programming in a new language or environment.
-
-The objectives are simple:
-
-- Write a function that returns the string "Hello, World!".
-- Run the test suite and make sure that it succeeds.
-- Submit your solution and check it at the website.
-
-If everything goes well, you will be ready to fetch your first real exercise. */
-
-function helloWorld()
-{
-    return "Hello, World!";
-}
+    function reverseString(string $text): string {
+        $result = strrev($text);
+        return $result;     
+    }
 ?>
